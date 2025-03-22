11fd8d7e4026d78f032ab3a5da52a4b3a9bad5f9
dehelixm <depthm@yandex.ru>
2025-03-22 17:00:47 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function openModal(title, description) {
-    document.getElementById('modal-title').textContent = title;
-    document.getElementById('modal-description').textContent = description;
-    document.getElementById('modal').style.display = 'flex';
-}
-
-function closeModal() {
-    document.getElementById('modal').style.display = 'none';
-}
